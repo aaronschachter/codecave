2d5eda69cd5c71ba309103c517f9107c3aca8e86
Aaron Schachter <aaronschachter@gmail.com>
2015-09-12 18:08:59 -0700

    Codecave changes

--- editor.php
+++ editor.php
@@ -1,16 +1,16 @@
 <div class="row">
-  <div class="col-xs-6">
+  <div class="col-xs-8">
     <h3>PHP</h3>
     <form action="" method="post" role="form">
       <div class="form-group">
-        <textarea name="content" class="form-control" rows="10"><?php print trim($_POST['content']); ?></textarea>
+        <textarea id="codecave" name="content" class="form-control" rows="10"><?php print trim($_POST['content']); ?></textarea>
       </div>
       <div>
         <button type="submit" class="btn btn-default">Execute</button>
       </div>
     </form>
   </div>
-  <div class="col-xs-6">
+  <div class="col-xs-4">
     <h3>Output</h3>
     <?php if (array_key_exists('content', $_POST)): ?>
       <pre><?php print eval($_POST['content']); ?></pre>
